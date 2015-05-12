9173b02315a0c4122216d2d9a4185e4417961db3
B-Brother <464248978@qq.com>
2015-05-12 23:36:26 +0800

    change invoke init method

--- tinker-network/src/main/java/com/alibaba/tinker/consumer/Consumer.java
+++ tinker-network/src/main/java/com/alibaba/tinker/consumer/Consumer.java
@@ -1,6 +1,19 @@
 package com.alibaba.tinker.consumer;
  
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 import java.lang.reflect.Proxy; 
+
+import com.alibaba.tinker.handler.RegisterCenterHandler;
+import com.alibaba.tinker.util.Host;
  
 public final class Consumer {  
      
@@ -17,9 +30,27 @@ public final class Consumer {
 		this.serviceName = serviceName;
 	} 
 
-	@SuppressWarnings("rawtypes")
+	/**
+	 * init初始化需要做的事情如下:
+	 * 
+	 * 1. 生成代理对象。
+	 * 2. 和注册中心(RC)建立起连接
+	 */ 
 	public void init(){
-		// 生成代理对象
+		buildProxyObject();
+		
+		buildConnection2RegisterCenter(serviceName);
+    }
+    
+    public Object getObject(){
+    	return proxy;
+    }  
+    
+    // ---- private
+    
+    @SuppressWarnings("rawtypes")
+	private void buildProxyObject(){
+    	// 生成代理对象
     	Class clazz;
 		try {
 			clazz = Class.forName(serviceName.substring(0, serviceName.lastIndexOf(":")));
@@ -43,9 +74,45 @@ public final class Consumer {
 		}  
     }
     
-    public Object getObject(){
-    	return proxy;
-    }  
+    /**
+     *  和注册中心(RC)建立连接
+     *  
+     *  通道建立时: 发出请求服务地址提供列表消息
+     *  RC回应: 地址列表信息，回写到ServiceAddressCache
+     */ 
+	private void buildConnection2RegisterCenter(final String serviceName){  
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				EventLoopGroup group = new NioEventLoopGroup();
+		        try {
+		            Bootstrap b = new Bootstrap();
+		            b.group(group)
+		             .channel(NioSocketChannel.class)
+		             .option(ChannelOption.TCP_NODELAY, true)
+		             .handler(new ChannelInitializer<SocketChannel>() {
+		                 @Override
+		                 public void initChannel(SocketChannel ch) throws Exception {
+		                     ChannelPipeline p = ch.pipeline();
+		             
+		                     //p.addLast(new LoggingHandler(LogLevel.INFO));
+		                     p.addLast(new RegisterCenterHandler(serviceName));
+		                     }
+		                 });
+		  
+		                ChannelFuture f = b.connect(Host.RC_HOST, 8007).sync(); 
+
+		            // Wait until the connection is closed.
+		            f.channel().closeFuture().sync(); 
+		        } catch (InterruptedException e) { 
+					e.printStackTrace();
+				} finally { 
+		            group.shutdownGracefully();
+		        }  
+			}
+		}).start();
+    }
 }
 
 
--- tinker-network/src/main/java/com/alibaba/tinker/consumer/TinkerInvokeHandler.java
+++ tinker-network/src/main/java/com/alibaba/tinker/consumer/TinkerInvokeHandler.java
@@ -168,47 +168,7 @@ public class TinkerInvokeHandler implements InvocationHandler {
 		}).start();
     }
     
-    /**
-     *  和注册中心(RC)建立连接
-     *  
-     *  通道建立时: 发出请求服务地址提供列表消息
-     *  RC回应: 地址列表信息，回写到ServiceAddressCache
-     */ 
-	private void buildConnection2RegisterCenter(final String serviceName){  
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				EventLoopGroup group = new NioEventLoopGroup();
-		        try {
-		            Bootstrap b = new Bootstrap();
-		            b.group(group)
-		             .channel(NioSocketChannel.class)
-		             .option(ChannelOption.TCP_NODELAY, true)
-		             .handler(new ChannelInitializer<SocketChannel>() {
-		                 @Override
-		                 public void initChannel(SocketChannel ch) throws Exception {
-		                     ChannelPipeline p = ch.pipeline();
-		             
-		                     //p.addLast(new LoggingHandler(LogLevel.INFO));
-		                     p.addLast(new ObjectEncoder(),
-		                               new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
-		                               new RegisterCenterHandler(serviceName));
-		                     }
-		                 });
-		  
-		                ChannelFuture f = b.connect(Host.RC_HOST, 8007).sync(); 
-
-		            // Wait until the connection is closed.
-		            f.channel().closeFuture().sync(); 
-		        } catch (InterruptedException e) { 
-					e.printStackTrace();
-				} finally { 
-		            group.shutdownGracefully();
-		        }  
-			}
-		}).start();
-    }
+    
     
 	/**
 	 *  第1-8个字节:    magicNumber: B-TINKER (my name :) )
